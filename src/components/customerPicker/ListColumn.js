86b3d36c0968dd1670966d5785bc91e8143263aa
Amalia Man <amaliagilad@gmail.com>
2018-12-19 08:46:22 +0200

    Refactor ListColumn

--- src/components/customerPicker/ListColumn.js
+++ src/components/customerPicker/ListColumn.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react';
 
 const ListItem = props => (
-    <div
-        onClick={() => props.selectItem(props.value)}
-        className={props.selectedItem === props.value ? 'selected' : null}
-    >
+    <div onClick={props.handleClick} className={props.cssClass}>
         {props.label}
     </div>
 );
 
 class ListColumn extends Component {
     render() {
-        const { data, isCompany } = this.props;
+        const { data, isCompany, selectedItem, selectItem } = this.props;
 
         return (
             <div className='list'>
                 {data && data.map(c => {
                     const value = isCompany ? c.id : c;
-                    const label = isCompany ? c.company : c;
                     return (
                         <ListItem
                             key={value}
-                            value={value}
-                            label={label}
-                            selectItem={this.props.selectItem}
-                            selectedItem={this.props.selectedItem}
+                            label={isCompany ? c.company : c}
+                            handleClick={() => selectItem(value)}
+                            cssClass={selectedItem === value ? 'selected' : null}
                         />
                     )
                 })}
