509089e8c126cab58f5766a8aa2fcbd1a6844836
Amalia Man <amaliagilad@gmail.com>
2018-12-20 09:56:30 +0200

    Updates

---
+++ precache-manifest.9348ee9f240290193b43f436813573c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "912f2b4ef31c459a9994",
+    "url": "/react-assignment/static/js/runtime~main.912f2b4e.js"
+  },
+  {
+    "revision": "70a8d7fdea548bbcc74a",
+    "url": "/react-assignment/static/js/main.70a8d7fd.chunk.js"
+  },
+  {
+    "revision": "909e1a2086dd122bbc50",
+    "url": "/react-assignment/static/js/1.909e1a20.chunk.js"
+  },
+  {
+    "revision": "70a8d7fdea548bbcc74a",
+    "url": "/react-assignment/static/css/main.dc1319e4.chunk.css"
+  },
+  {
+    "revision": "2604a62fc627f2cbffdc9abec5dffdd4",
+    "url": "/react-assignment/index.html"
+  }
+];
